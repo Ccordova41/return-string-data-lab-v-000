fdd3c0445b53303115c42e6f2dcf3b29d821a474
Catherine Cordova <ccordova41@gmail.com>
2017-12-05 20:06:39 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,16 @@ class ProductsController < ApplicationController
         render :new
   end
 
+  def inventory
+     product = Product.find(params[:id])
+     render plain: product.inventory > 0 ? 'true' : 'false'
+   end
+
+  def description
+     product = Product.find(params[:id])
+     render plain: product.description
+  end
+
   private
 
   def product_params
