227252b94657cb51c975bc263d8449aa471bb067
Catherine Cordova <ccordova41@gmail.com>
2017-12-05 20:05:11 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+  end
+
+  def new 
+    @product = Product.new 
   end 
 
-  
+  def create 
+    product = Product.new(product_params)
+  end 
+
+  private 
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
+  end  
 end
